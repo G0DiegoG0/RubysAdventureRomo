44327e29828562d27b7cd051d1c1c5ff299a8601
G0DiegoG0 <116124417+G0DiegoG0@users.noreply.github.com>
2022-12-07 15:27:21 -0800

    added health
    gave ruby health and decoration

--- RubysAdventureRomoP7/Assets/scripts/Rubycontroller.cs
+++ RubysAdventureRomoP7/Assets/scripts/Rubycontroller.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rubycontroller : MonoBehaviour
+public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+
     public int maxHealth = 5;
+
+    public int health { get } return currentHealth; }}
     int currentHealth;
 
     Rigidbody2D rigidbody2d;
@@ -29,13 +33,13 @@ public class Rubycontroller : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
     }
 
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
